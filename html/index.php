46ddd685e519a22d93a447c8beadac396f3d1976
vitliemova <vitliemova@gmail.com>
2025-11-12 18:10:57 +0200

    Update index.php

--- html/index.php
+++ html/index.php
@@ -6,7 +6,7 @@
   <body>
     <div align="center">
       <img width="340px" height="340px" src="kubernetes-logo.png">
-      <h1>Hello world!! Welcome to the world of <span style="color: blue;">Kubernetes!</span>!</h1>
+      <h1>Hello world!! Welcome to the world of <span style="color: blue;">Kubernetes</span>!</h1>
       <h2>There is plenty to explore here ...</h2>
       <h2>You can start with the name: <span style="color: blue;">κυβερνήτης</span> &rArr; <span style="color: blue;">kubernetes</span> &rArr; <span style="color: blue;">k8s</span> :)</h2>
       <h2>And then continue the journey with </h2><img src="minikube-logo.png">
